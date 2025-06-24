d3c7a1cc615fdcb5f94fa9c32f1a759186247fde
kubo6472 <26146502+kubo6472@users.noreply.github.com>
2025-06-24 20:33:46 +0200

    Update BridgeInstance.tsx

--- app/components/BridgeInstance.tsx
+++ app/components/BridgeInstance.tsx
@@ -1,16 +1,17 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default function BridgeInstance({name, onFly, beeperToken, flyToken, onDelete}: any) {
+export default function BridgeInstance({ name, onFly, beeperToken, flyToken, onDelete }: any) {
 
     const [deleteInProgress, setDeleteInProgress] = useState(false)
     const [errorMessage, setErrorMessage] = useState("")
+    const [redeploying, setRedeploying] = useState(false)
 
     async function deleteBridge() {
         setDeleteInProgress(true);
 
         const res = await fetch("/api/delete", {
             method: 'DELETE',
-            body: JSON.stringify({beeperToken: beeperToken, flyToken: flyToken, name: name, onFly: onFly})
+            body: JSON.stringify({ beeperToken: beeperToken, flyToken: flyToken, name: name, onFly: onFly })
         })
 
         if (res.status === 500) {
@@ -23,6 +24,29 @@ export default function BridgeInstance({name, onFly, beeperToken, flyToken, onDe
         onDelete();
     }
 
+    async function redeployBridge() {
+        setRedeploying(true)
+
+        const res = await fetch("/api/deploy", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                redeploy: true,
+                appName: name,
+                flyToken: flyToken
+            })
+        })
+
+        if (res.status !== 200) {
+            const error_data = await res.json()
+            setErrorMessage(error_data.error)
+        }
+
+        setRedeploying(false)
+    }
+
     return (
         <tr>
             <td className="border p-2">
@@ -30,17 +54,28 @@ export default function BridgeInstance({name, onFly, beeperToken, flyToken, onDe
             </td>
 
             <td className="border p-2 text-center">
-                { onFly && <button className="p-2 rounded-md m-4 bg-gray-600 border-0 text-white hover:bg-gray-500" onClick={() => { navigator.clipboard.writeText(`@${name}bot:beeper.local`)}}>Copy</button>}
+                {onFly && <button className="p-2 rounded-md m-4 bg-gray-600 border-0 text-white hover:bg-gray-500" onClick={() => { navigator.clipboard.writeText(`@${name}bot:beeper.local`) }}>Copy</button>}
             </td>
 
             <td className="border p-2 text-center">
-                { onFly && <a target="_blank" href={`https://fly.io/apps/${name}/monitoring`} rel="noopener noreferrer">View on Fly</a> }
+                {onFly && <a target="_blank" href={`https://fly.io/apps/${name}/monitoring`} rel="noopener noreferrer">View on Fly</a>}
             </td>
 
             <td className="border p-2 text-center">
-                {!deleteInProgress ? <button className="p-2 rounded-md m-4 bg-red-600 border-0 text-white hover:bg-red-500" onClick={deleteBridge}>Delete</button> :
-                    <button className="p-2 rounded-md m-4 bg-red-300 border-0 text-white" disabled={true}>Deleting...</button>}
-                { errorMessage }
+                {!deleteInProgress ? (
+                    <button className="p-2 rounded-md m-4 bg-red-600 border-0 text-white hover:bg-red-500" onClick={deleteBridge}>Delete</button>
+                ) : (
+                    <button className="p-2 rounded-md m-4 bg-red-300 border-0 text-white" disabled={true}>Deleting...</button>
+                )}
+                {errorMessage}
+            </td>
+
+            <td className="border p-2 text-center">
+                {!redeploying ? (
+                    <button className="p-2 rounded-md m-4 bg-blue-600 border-0 text-white hover:bg-blue-500" onClick={redeployBridge}>Redeploy</button>
+                ) : (
+                    <button className="p-2 rounded-md m-4 bg-blue-300 border-0 text-white" disabled={true}>Redeploying...</button>
+                )}
             </td>
         </tr>
     )
